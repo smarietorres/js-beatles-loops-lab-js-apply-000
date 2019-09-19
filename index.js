895c0ad23c83e78e4b70f11eeffd968b90ecc721
Stephanie Torres <marietorresny@gmail.com>
2019-09-19 14:10:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments) {
+  var x = []
+  for (var i = 0; i < musicians.length; i++) {
+    x.push(musicians[i] + ' plays ' + instruments[i])
+  }
+  return x;
+}
